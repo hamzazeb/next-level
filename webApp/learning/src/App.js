6dd59be6149051a4f18a64ca24f2447b3b4458e4
humnakashif <humnakashif9@gmail.com>
2021-08-14 17:15:52 +0500

    Components and Structures

--- webApp/learning/src/App.js
+++ webApp/learning/src/App.js
@@ -1,28 +1,12 @@
 import logo from './logo.svg';
-// import {p} from './Components/Paraghraph'
 import Paraghraph from './Components/Paraghraph'
-import './App.css';
+import routes from './Components/config/routes';
+import './App.css'
+import Login from './Components/login/container/login';
+import Routes from './Components/config/routes';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* {p()} */}
-        <Paraghraph name="hamaza"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Paraghraph name="Farid"/>
-        {/* {p()} */}
-      </header>
-    </div>
-  );
+  return <Routes/>
 }
 
 export default App;
---
+++ webApp/learning/src/Components/config/routes.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {Container} from 'react-bootstrap';
+import Login from '../login/container/login';
+import Register from '../register/container/register';
+import Passwords from '../passwords/container/password';
+
+const Routes=()=>{
+    return(
+        <Fragment>
+            <BrowserRouter>
+                <Switch>
+                    <Container> 
+                        <Route exact path="/" component={Login}/>
+                        <Route exact path="/register" component={Register}/>
+                        <Route exact path="/recover/password" component={Passwords}/>
+                    </Container>
+                </Switch>
+            </BrowserRouter>
+        </Fragment>
+    )
+}
+export default Routes
